f0a982f9147f41f8a7d0884b90d8acbd55965256
kaikeUmobi <kaike@umobi.com.br>
2020-06-30 11:31:01 -0300

    Rota de teste

---
+++ database/seeds/TestSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('test')->insert(
+            ['message' => "hello world!"]
+        );
+    }
+}
